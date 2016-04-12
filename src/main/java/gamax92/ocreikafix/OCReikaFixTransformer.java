fccf45a7f9260f4e4307781040c9fffb000f116f
gamax92 <gamax92@aol.com>
2016-04-11 22:17:56 -0600

    Demote visibility in existing blocks
    Patch TileEntityBase.readFromNBT to automatically demote all blocks that
    are Visibility.Network or higher (not that there is a higher)

--- src/main/java/gamax92/ocreikafix/OCReikaFixTransformer.java
+++ src/main/java/gamax92/ocreikafix/OCReikaFixTransformer.java
@@ -7,10 +7,7 @@ import org.apache.logging.log4j.Level;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.FieldInsnNode;
-import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.*;
 
 import net.minecraft.launchwrapper.IClassTransformer;
 
@@ -56,12 +53,82 @@ public class OCReikaFixTransformer implements IClassTransformer {
 		return bytes;
 	}
 
+	private byte[] transformPatchNBT(String name, String transformedName, byte[] bytes) {
+		OCReikaFixCore.logMessage(Level.INFO, "Patching class: " + transformedName);
+
+		ClassReader cr = new ClassReader(bytes);
+		ClassNode cn = new ClassNode(Opcodes.ASM4);
+		cr.accept(cn, 0);
+
+		boolean patched = false;
+
+		for (MethodNode mn : cn.methods) {
+			if ((mn.name.equals("readFromNBT") || mn.name.equals("func_145839_a")) && mn.desc.equals("(Lnet/minecraft/nbt/NBTTagCompound;)V")) {
+				OCReikaFixCore.logMessage(Level.INFO, "Patching method: " + mn.name);
+				boolean clean = mn.name.equals("readFromNBT");
+
+				Iterator<AbstractInsnNode> iter = mn.instructions.iterator();
+				while (iter.hasNext()) {
+					AbstractInsnNode currentNode = iter.next();
+
+					if (currentNode instanceof TypeInsnNode && currentNode.getOpcode() == Opcodes.INSTANCEOF) {
+						System.out.println("Found patching location!");
+
+						AbstractInsnNode test = iter.next();
+
+						if (test instanceof JumpInsnNode && test.getOpcode() == Opcodes.IFEQ) {
+							InsnList toInject = new InsnList();
+
+							toInject.add(new LabelNode(null));
+							toInject.add(new VarInsnNode(Opcodes.ALOAD, 1));
+							toInject.add(new LdcInsnNode("visibility"));
+							toInject.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "net/minecraft/nbt/NBTTagCompound", clean ? "hasKey" : "func_74764_b", "(Ljava/lang/String;)Z"));
+							LabelNode LacksKey = new LabelNode(null);
+							toInject.add(new JumpInsnNode(Opcodes.IFEQ, LacksKey));
+							toInject.add(new LabelNode(null));
+							toInject.add(new VarInsnNode(Opcodes.ALOAD, 1));
+							toInject.add(new LdcInsnNode("visibility"));
+							toInject.add(new VarInsnNode(Opcodes.ALOAD, 1));
+							toInject.add(new LdcInsnNode("visibility"));
+							toInject.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "net/minecraft/nbt/NBTTagCompound", clean ? "getInteger" : "func_74762_e", "(Ljava/lang/String;)I"));
+							toInject.add(new InsnNode(Opcodes.ICONST_1));
+							toInject.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "java/lang/Math", "min", "(II)I"));
+							toInject.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "net/minecraft/nbt/NBTTagCompound", clean ? "setInteger" : "func_74768_a", "(Ljava/lang/String;I)V"));
+							toInject.add(LacksKey);
+
+							mn.instructions.insert(test, toInject);
+
+							OCReikaFixCore.logMessage(Level.INFO, mn.name + " has been patched!");
+							patched = true;
+
+							break;
+						} else
+							OCReikaFixCore.logMessage(Level.ERROR, "Instruction check failed!");
+					}
+				}
+			}
+		}
+
+		ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+		cn.accept(cw);
+		if (patched) {
+			OCReikaFixCore.logMessage(Level.INFO, "Method was successfully patched!");
+			bytes = cw.toByteArray();
+		} else {
+			OCReikaFixCore.logMessage(Level.ERROR, "Failed to patch TileEntityBase.readFromNBT");
+			OCReikaFixCore.logMessage(Level.ERROR, "Visibility will not update unless blocks are broken and put back down.");
+		}
+
+		return bytes;
+	}
+
 	@Override
 	public byte[] transform(String name, String transformedName, byte[] bytes) {
 		if (bytes != null) {
-			if (classes.contains(transformedName)) {
+			if (transformedName.equals("Reika.DragonAPI.Base.TileEntityBase"))
+				bytes = transformPatchNBT(name, transformedName, bytes);
+			if (classes.contains(transformedName))
 				return transformPatchVisibility(name, transformedName, bytes);
-			}
 		}
 		return bytes;
 	}
